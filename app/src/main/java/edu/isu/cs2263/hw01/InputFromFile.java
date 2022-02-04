3a11fba99b5cb20c956d778efb6a64e1019cdab4
Zackary Beck <beckzack@isu.edu>
2022-02-03 19:40:21 -0700

    Updated the changelog and the README

--- app/src/main/java/edu/isu/cs2263/hw01/InputFromFile.java
+++ app/src/main/java/edu/isu/cs2263/hw01/InputFromFile.java
@@ -6,23 +6,25 @@ import java.util.Scanner;
 /**
  * A Java class that reads single-line expressions from a text file and sends them to be evaluated
  */
-public class InputFromFile {
+public class InputFromFile implements InputInterface {
     /**
      * Takes the file name/path given in the command line arguments and sends to it be read
      *
      * @param filePath Path/name of the file given in the command line arguments
      */
+
+    String filePath;
+
     public InputFromFile(String filePath) {
-        readFromFile(filePath);
+        this.filePath = filePath;
+        read();
     }
 
     /**
      * Reads each line in the given file and sends them to the evaluate class to be evaluated.
      * If the user specified file path cannot be found a FileNotFoundException is thrown by the File class
-     *
-     * @param filePath Path/name of the file given in the command line arguments
      */
-    public void readFromFile(String filePath) {
+    public void read() {
         try {
             File file = new File(filePath);
             Scanner scanner = new Scanner(file);
---
+++ app/src/main/java/edu/isu/cs2263/hw01/InputInterface.java
@@ -0,0 +1,13 @@
+package edu.isu.cs2263.hw01;
+
+/**
+ * A simple interface for getting input
+ *
+ * @author Zackary Beck
+ */
+public interface InputInterface {
+    /**
+     * Either uses an infinite loop to get input from the user or reads the input from a given text file
+     */
+    void read();
+}
